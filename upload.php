8a02eaa7c1a53527f36bb8f2af3da5b83ba18053
Anders Saints <anders.lsantos@gmail.com>
2022-05-02 15:07:55 -0300

    ajustado front dos videos

--- upload.php
+++ upload.php
@@ -5,11 +5,15 @@ if (!isset($_SESSION["nome_usuario"])) {
   die;
 }
 
+$success = 0;
+$erro = 0;
+
 include("db.class.php");
 
 $objDb = new db();
 $link = $objDb->conecta_mysql();
 
+$idusers = $_SESSION["id_usuario"];
 
 if (isset($_FILES['video'])) {
   $arquivo = $_FILES['video'];
@@ -28,21 +32,34 @@ if (isset($_FILES['video'])) {
   $extension = strtolower(pathinfo($nomeDoArquivo, PATHINFO_EXTENSION));
   $extensions_arr = array("mp4", "avi", "3gp", "mov", "mpeg");
 
+
+
   if (in_array($extension, $extensions_arr)) {
 
     $path = $videos_dir . $novoNomeDoArquivo . "." . $extension;
 
     $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
-  }
-  $sql = "INSERT INTO videos (name, location) VALUES('$nomeDoArquivo','$path')";
 
-  if ($deu_certo) {
-    mysqli_query($link, $sql);
-    echo "<p>Arquivo enviado com sucesso!</p>";
+    $sql = "INSERT INTO videos (idusers, name, location) VALUES($idusers,'$nomeDoArquivo','$path')";
+
+    if ($deu_certo) {
+      mysqli_query($link, $sql);
+      $success = 1;
+    } else {
+      echo 'Falha no envio!.' . mysqli_connect_error();
+    }
   } else {
-    echo 'Erro ao registrar o usuário!.' . mysqli_connect_error();
+    $erro = 1;
   }
 }
+
+$sql = " SELECT * FROM videos WHERE idusers = $idusers";
+
+$results_id = mysqli_query($link, $sql);
+
+$user_data = mysqli_fetch_all($results_id);
+
+
 
 ?>
 
@@ -71,10 +88,51 @@ if (isset($_FILES['video'])) {
     </h1>
     <form action="" method="POST" enctype='multipart/form-data'>
       <div class="input-group my-5">
-        <input type="file" class="form-control" aria-label="Upload" name="video" required />
+        <input type="file" class="form-control" aria-label="Upload" name="video" accept="video/*" required />
         <button class="btn btn-outline-secondary" type="submit">Enviar</button>
       </div>
+      <?php
+      if ($success == 1) {
+        echo '<div class="row">
+                        <div class="alert alert-success" id="success-alert">
+                            <label class="badge bg-success">Success! </label> Vídeo enviado com sucesso!
+                        </div>
+                    </div>';
+      }
+      if ($erro == 1) {
+        echo '<div class="row">
+        <div class="alert alert-danger" id="error-alert">
+            <label class="badge bg-danger">Erro! </label> Arquivo inválido!
+        </div>
+    </div>';
+      }
+      ?>
     </form>
+  </div>
+  <div class="container">
+    <div class="row">
+      <!-- <div class="card-group"> -->
+
+        <?php
+        foreach ($user_data as $dados) {
+
+        ?>
+          <div class="card col-md-4 col-sm-12 mx-auto">
+            <video height="240" src="<?php echo $dados[3]; ?>" type="video/mp4" controls controlsList="nodownload" class="videoplayer"></video>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <div class="row">
+                <a onclick="<?php  ?>" href="#" style="width: 6rem;" class="btn btn-danger col-sm-5 mx-auto">Delete</a>
+                <a onclick="share()" href="#" style="width: 6rem;" class="btn btn-primary col-sm-5 mx-auto">Share</a>
+              </div>
+            </div>
+          </div>
+
+        <?php
+        }
+        ?>
+      <!-- </div> -->
+    </div>
   </div>
 
 
@@ -108,11 +166,25 @@ if (isset($_FILES['video'])) {
     setTimeout(sumir, 3000)
 
     function sumir() {
+      $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+        $("#success-alert").slideUp(500);
+      });
       $("#error-alert").fadeTo(2000, 500).slideUp(500, function() {
         $("#error-alert").slideUp(500);
       });
     }
+    document.getElementsByClassName("videoplayer").addEventListener("playing", event => {
+      const player = document.getElementsByClassName("videoplayer");
+      if (player.requestFullscreen)
+        player.requestFullscreen();
+      else if (player.webkitRequestFullscreen)
+        player.webkitRequestFullscreen();
+      else if (player.msRequestFullScreen)
+        player.msRequestFullScreen();
+      dd
+    })
   </script>
+
 </body>
 
 </html>
